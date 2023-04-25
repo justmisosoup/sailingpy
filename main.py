549fe87fa2b215c3401b3d8fbc208bd4c2538f14
Sara <4011383+justmisosoup@users.noreply.github.com>
2023-04-25 16:42:29 -0700

    test

--- main.py
+++ main.py
@@ -20,46 +20,14 @@ def ships_in_san_francisco_bay(records: RecordList) -> RecordList:
         [record for record in records if record.value["staticData"]["shipType"] == "SAILING" and is_in_san_francisco_bay(record)]
     )
 
-
 class App:
     @staticmethod
     async def run(turbine: Runtime):
         try:
-            # To configure your data stores as resources on the Meroxa Platform
-            # use the Meroxa Dashboard, CLI, or Meroxa Terraform Provider.
-            # For more details refer to: https://docs.meroxa.com/
-
-            # Identify an upstream data store for your data app
-            # with the `resources` function.
-            # Replace `source_name` with the resource name the
-            # data store was configured with on the Meroxa platform.
             source = await turbine.resources("my-spire")
-
-            # Specify which upstream records to pull
-            # with the `records` function.
-            # Replace `collection_name` with a table, collection,
-            # or bucket name in your data store.
-            # If you need additional connector configurations, replace '{}'
-            # with the key and value, i.e. {"incrementing.field.name": "id"}
             records = await source.records("")
-
-            # Specify what code to execute against upstream records
-            # with the `process` function.
-            # Replace `anonymize` with the name of your function code.
             sailing = await turbine.process(records, ships_in_san_francisco_bay)
-
-            # Identify a downstream data store for your data app
-            # with the `resources` function.
-            # Replace `destination_name` with the resource name the
-            # data store was configured with on the Meroxa platform.
             destination_db = await turbine.resources("sailingwh")
-
-            # Specify where to write records downstream
-            # using the `write` function.
-            # Replace `collection_archive` with a table, collection,
-            # or bucket name in your data store.
-            # If you need additional connector configurations, replace '{}'
-            # with the key and value, i.e. {"behavior.on.null.values": "ignore"}
             await destination_db.write(sailing, "")
         except Exception as e:
             print(e, file=sys.stderr)
