b86316e39e72ef246a47abd3f49cf19f3b1e280c
Sara <4011383+justmisosoup@users.noreply.github.com>
2023-04-28 12:56:40 -0700

    test

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from typing import List
 from turbine.runtime import Record, RecordList
 from turbine.runtime import Runtime
 
-def ships_in_san_francisco_bay(records: RecordList) -> RecordList:
+""" def ships_in_san_francisco_bay(records: RecordList) -> RecordList:
 
     # This function takes a RecordList as input and returns a filtered list of records with shipType 'SAILING' and
     # lastPositionUpdate latitude and longitude data that puts them in the San Francisco Bay Area Waters.
@@ -25,7 +25,21 @@ def ships_in_san_francisco_bay(records: RecordList) -> RecordList:
                 rl.append(record)
     except Exception as e:
         print(e, file=sys.stderr)
-    return rl
+    return rl """
+
+def ships_in_san_francisco_bay(records: RecordList) -> RecordList:
+
+    # This function takes a RecordList as input and returns a filtered list of records with shipType 'SAILING' and
+    # lastPositionUpdate latitude and longitude data that puts them in the San Francisco Bay Area Waters.
+
+    def is_in_san_francisco_bay(record: Record) -> bool:
+        latitude = record.value["lastPositionUpdate"]["latitude"]
+        longitude = record.value["lastPositionUpdate"]["longitude"]
+        return 37.5 <= latitude <= 38.2 and -123.0 <= longitude <= -122.2
+
+    return RecordList(
+        [record for record in records if record.value["staticData"]["shipType"] == "SAILING" and is_in_san_francisco_bay(record)]
+    )
 
 class App:
     @staticmethod
