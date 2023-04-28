08095df21d54502fb70bd869098fd295e476a9f2
Sara <4011383+justmisosoup@users.noreply.github.com>
2023-04-28 12:32:24 -0700

    remove payload

--- main.py
+++ main.py
@@ -12,22 +12,15 @@ def ships_in_san_francisco_bay(records: RecordList) -> RecordList:
     # lastPositionUpdate latitude and longitude data that puts them in the San Francisco Bay Area Waters.
 
     def is_in_san_francisco_bay(record: Record) -> bool:
-        payload = record.value["payload"]
-        if record.value["after"] is not None:
-            payload = record.value["after"]
-        latitude = payload["lastPositionUpdate"]["latitude"]
-        longitude = payload["lastPositionUpdate"]["longitude"]
+        latitude = record.value["lastPositionUpdate"]["latitude"]
+        longitude = record.value["lastPositionUpdate"]["longitude"]
         return 37.5 <= latitude <= 38.2 and -123.0 <= longitude <= -122.2
 
     rl = RecordList()
     
     try:
         for record in records:
-            
-            payload = record.value["payload"]
-            if record.value["after"] is not None:
-                payload = record.value["after"]
-            if payload["staticData"]["shipType"]  == "SAILING" and is_in_san_francisco_bay(record):
+            if record.value["staticData"]["shipType"]  == "SAILING" and is_in_san_francisco_bay(record):
                 rl.append(record)
     except Exception as e:
         print(e, file=sys.stderr)
